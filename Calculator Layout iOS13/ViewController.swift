67ffa7e8677b06913e31cbd49c7e2400cdd973cd
flyingking888 <jeremyfeiwang@outlook.com>
2019-10-29 10:42:50 +0000

    Update solution README

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -5,6 +5,9 @@
 //  Created by Angela Yu on 01/07/2019.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
+//  Enhanced by Jeremy Wang on 2/28/2023
+//  Features: Basic math calculator
+
 
 import UIKit
 
